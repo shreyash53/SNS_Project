983b8937ea4233b170c7301bfb1121e5a7a73b4c
Shreyash Agrawal <35464275+sensei23@users.noreply.github.com>
2022-03-27 12:25:29 +0530

    forgot and reset apis added

--- insecure_application/config.py
+++ insecure_application/config.py
@@ -1,6 +1,8 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
+from flask_mail import Mail
 
 db = SQLAlchemy()
 session_obj = Session()
+mail = Mail()
--- insecure_application/utilities/common_functions.py
+++ insecure_application/utilities/common_functions.py
@@ -1,6 +1,16 @@
 
 from flask import session
+from config import mail
+from flask_mail import Message
+from utilities.constants import SENDER_EMAIL
 
 def check_session_exists():
     return session and 'name' in session and session['name']
-     
+
+def email_service(user_email, email_title, user_id):
+    try:
+        msg = Message(email_title, sender = SENDER_EMAIL, recipients = [user_email])
+        msg.body = "Hello, Reset your account using this url: " + 'http://localhost:5000' + '/user/pass_reset' + '/%d'%(user_id)
+        mail.send(msg)
+    except:
+        print('mail failed to send')
